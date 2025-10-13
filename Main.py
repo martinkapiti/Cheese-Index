0d361d4f4ff4bd92028cd38b71ab87b2bde73911
BrianM2006 <bm44862p@pace.edu>
2025-10-13 13:02:29 -0400

    Update Main.py
    added some dicts

--- Main.py
+++ Main.py
@@ -1,1 +1,2 @@
-im gonna add something here later lol
+walmart, stopAndShop = {"Doritos": 1.23, "Oreo": 2.76, "Goldfish": 2.48, "Reese's": 1.31}, {"Doritos": 1.26, "Oreo": 2.54, "Goldfish": 1.06, "Reese's": 2.06}
+target, shopRite = {"Doritos": 1.45, "Oreo": 1.26, "Goldfish": 1.55, "Reese's": 1.96}, {"Doritos": 1.33, "Oreo": 2.93, "Goldfish": 2.88, "Reese's": 2.97}
